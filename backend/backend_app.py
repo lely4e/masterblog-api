a54bf01d94d558d086b547a6e702555184098e9b
Olga Chirkova <lely4e@gmail.com>
2025-09-18 12:40:24 +0200

    Handling input validation

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -9,44 +9,63 @@ POSTS = [
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
 
+
 def get_id():
-    """ Gets the maximum id in posts and generate a new one by adding 1 """
-    return max((post.get("id") for post in POSTS if isinstance(post.get("id"), int)), default=0) + 1
+    """Gets the maximum id in posts and generate a new one by adding 1"""
+    return (
+        max(
+            (post.get("id") for post in POSTS if isinstance(post.get("id"), int)),
+            default=0,
+        )
+        + 1
+    )
 
 
 # @app.route('/api/posts', methods=['GET'])
 # def get_posts():
 #     return jsonify(POSTS)
 
+
 def validate_posts_data(data):
+    """Input Validation from the user"""
     if "title" not in data or "content" not in data:
+        return False
+    if not data["title"].strip() or not data["content"].strip():
         return False
     return True
 
 
-@app.route('/api/posts', methods=['GET','POST'])
+@app.route("/api/posts", methods=["GET", "POST"])
 def get_posts():
-    if request.method == 'POST':
+    """
+    Add a new post to database.
+
+    GET: Returns all posts
+    POST: Adds a new post if input is valid.
+    """
+    if request.method == "POST":
         # Get new post data from client
         new_post = request.get_json()
 
         # Input Validation
-        if not new_post or not validate_posts_data(new_post):
-            return jsonify({"error": "Invalid post data"}), 400
+        if not new_post:
+            return (jsonify({"error": "Empty post data"}), 400)
+        if not validate_posts_data(new_post):
+            return (jsonify({"error": "Empty title or content"}), 400)
 
-        # Add new id 
+        # Add new id
         new_id = get_id()
         new_post["id"] = new_id
-        
+
         # Add a new post
         POSTS.append(new_post)
 
         # Return a post
         return jsonify(new_post), 201
 
-        # Return a post
+    # GET: Return all posts
     return jsonify(POSTS)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
